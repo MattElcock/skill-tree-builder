c606390ef3bee4f98e607761f5eaba797f3c906f
Matt <matthew.elcock82@googlemail.com>
2025-09-09 20:56:10 +0100

    chore: bootstrap react flow (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import "./App.css";
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type OnNodesChange,
+  type Node,
+  type Edge,
+  type OnEdgesChange,
+  type OnConnect,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
-function App() {
+const initialNodes: Node[] = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 50, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges: Edge[] = [
+  {
+    id: "n1-n2",
+    source: "n1",
+    target: "n2",
+    domAttributes: { "aria-description": "Hello world" },
+  },
+];
+
+export default function App() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect: OnConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
     </div>
   );
 }
-
-export default App;
