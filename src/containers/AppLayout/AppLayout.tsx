aea89a3db9f98099ff4d3fee41252ebf0791294e
Matt <matthew.elcock82@googlemail.com>
2025-09-11 20:59:01 +0100

    feat: list nodes (#7)

--- src/containers/AppLayout/AppLayout.tsx
+++ src/containers/AppLayout/AppLayout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import { EditNodes } from "../EditNodes";
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -9,14 +10,16 @@ const Sidebar = () => {
     <div className="bg-zinc-950 px-6 py-8 flex flex-col gap-10">
       <header>
         <h1 className="text-3xl">Skill Tree Builder</h1>
+        <p className="text-zinc-300">Build your skill tree</p>
       </header>
+      <EditNodes />
     </div>
   );
 };
 
 const AppLayout = ({ children }: AppLayoutProps) => {
   return (
-    <div className="h-dvh w-dvw grid grid-cols-[20rem_auto] text-white">
+    <div className="h-dvh w-dvw grid grid-cols-[20rem_auto] text-zinc-50">
       <Sidebar />
       <main>{children}</main>
     </div>
