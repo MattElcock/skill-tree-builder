08c0b6d3d351ca263607142f0d4d838206e1e9fa
Matt Elcock <matthew.elcock82@googlemail.com>
2025-09-12 14:53:15 +0100

    feat: move nodes

--- src/containers/SkillTree/SkillTree.tsx
+++ src/containers/SkillTree/SkillTree.tsx
@@ -1,9 +1,11 @@
-import { ReactFlow } from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
+import { useEditNodes } from "@/hooks/useEditNodes";
 import { useSkillTree } from "@/hooks/useSkillTree";
+import { applyNodeChanges, ReactFlow, type OnNodesChange } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 const SkillTree = () => {
   const { data: tree } = useSkillTree();
+  const { mutate } = useEditNodes();
 
   if (!tree) {
     return <p>Loading</p>;
@@ -11,7 +13,19 @@ const SkillTree = () => {
 
   const { nodes, edges } = tree;
 
-  return <ReactFlow colorMode="dark" nodes={nodes} edges={edges} fitView />;
+  const onNodesChange: OnNodesChange = (changes) => {
+    mutate(applyNodeChanges(changes, nodes));
+  };
+
+  return (
+    <ReactFlow
+      colorMode="dark"
+      nodes={nodes}
+      edges={edges}
+      fitView
+      onNodesChange={onNodesChange}
+    />
+  );
 };
 
 export { SkillTree };
---
+++ src/hooks/useEditNodes/index.ts
@@ -0,0 +1,1 @@
+export { useEditNodes } from "./useEditNodes";
